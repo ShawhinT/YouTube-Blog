6d2acd79d0ea70ad31a8bee10f8104eeb6d868d5
ShawhinT <shawhin@Shawhins-Mac-mini.local>
2023-08-03 08:22:38 -0500

    adding hugging face examples

---
+++ LLMs/hugging-face/my-first-space/README.md
@@ -0,0 +1,13 @@
+---
+title: My First Space
+emoji: 🐠
+colorFrom: pink
+colorTo: yellow
+sdk: gradio
+sdk_version: 3.39.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+Link to app: https://huggingface.co/spaces/shawhin/my-first-space
---
+++ LLMs/hugging-face/my-first-space/app.py
@@ -0,0 +1,17 @@
+from transformers import pipeline, Conversation
+import gradio as gr
+
+chatbot = pipeline(model="facebook/blenderbot-400M-distill")
+
+message_list = []
+response_list = []
+
+def vanilla_chatbot(message, history):
+    conversation = Conversation(text=message, past_user_inputs=message_list, generated_responses=response_list)
+    conversation = chatbot(conversation)
+
+    return conversation.generated_responses[-1]
+
+demo_chatbot = gr.ChatInterface(vanilla_chatbot, title="Vanilla Chatbot", description="Enter text to start chatting.")
+
+demo_chatbot.launch()
